86a77d0ba89461e11341220d45dfc75029e3a768
__filename <filename@inft.kr>
2023-01-08 19:52:34 +0800

    chore(appdir): added tailwind content app dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    './src/pages/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
